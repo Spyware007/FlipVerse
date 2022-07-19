bd31e9bc558d1ea04b78ef21a945187784695dd4
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-07-19 13:31:55 +0530

    Added function to logout from all devices

--- server/routes/userAuth.js
+++ server/routes/userAuth.js
@@ -1,11 +1,21 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.js";
-import { createUser, loginUser, logoutUser } from "../controllers/userAuth.js";
+import {
+	getAllUsers,
+	createUser,
+	loginUser,
+	logoutUserFromAllDevices,
+	logoutUser,
+} from "../controllers/userAuth.js";
 
 const userRouter = express.Router();
 
+userRouter.route("/api/user").post(getAllUsers);
 userRouter.route("/api/user/signup").post(createUser);
 userRouter.route("/api/user/login").post(loginUser);
 userRouter.route("/api/user/logout").post(authMiddleware, logoutUser);
+userRouter
+	.route("/api/user/logout/all")
+	.post(authMiddleware, logoutUserFromAllDevices);
 
 export default userRouter;
