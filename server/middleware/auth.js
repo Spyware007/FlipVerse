c461f33032dd60d615ee8718f8728c0685706dad
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-07-19 10:02:29 +0530

    Implemented user middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+const authMiddleware = async (req, res, next) => {
+	const token = req.header("x-auth-token");
+
+	try {
+		const decodedUser = await jwt.verify(token, process.env.SECRET_KEY);
+
+		const user = await User.findOne({
+			_id: decodedUser._id,
+			"tokens.token": token,
+		});
+
+		if (!user) {
+			throw new Error("Could not find user");
+		}
+
+		req.token = token;
+		req.user = user;
+		next();
+	} catch (error) {
+		console.log(error);
+		res.status(500).send({ error: "User not found" });
+	}
+};
+
+export default authMiddleware;
