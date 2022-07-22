2109a5f74436137331f26804810313919ace8f52
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-07-23 03:13:56 +0530

    modal code added

---
+++ client/src/Components/UI/Modal/Modal.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      <div className={classes.container}>
+        {ReactDOM.createPortal(
+          <ModalOverlay onColor={props.onColor}>{props.children}</ModalOverlay>,
+          portalElement
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Modal;
