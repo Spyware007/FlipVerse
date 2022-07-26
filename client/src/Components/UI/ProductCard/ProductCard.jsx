34b4219f3e68616334e67911018344fd4e41d5a9
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-07-27 02:11:30 +0530

    create product form updated

--- client/src/Components/UI/ProductCard/ProductCard.jsx
+++ client/src/Components/UI/ProductCard/ProductCard.jsx
@@ -5,6 +5,8 @@ import rs_icon from "../../../Assets/rs_icon.png";
 
 const ProductCard = ({
   image,
+  name,
+  price,
   outerWidth = "300px",
   outerHeight = "390px",
   innerWidth = "230px",
@@ -17,12 +19,10 @@ const ProductCard = ({
           <Card width={innerWidth} height={innerHeight}>
             <img className={classes.image} src={image} alt="product_image" />
           </Card>
-          <h4 className={classes.name}>
-            Demon Slayer's Hoddie Slayer's Hoddie
-          </h4>
+          <h4 className={classes.name}>{name}</h4>
           <div className={classes.price_container}>
             <img className={classes.price_symbol} src={rs_icon} alt="rsicon" />
-            <span className={classes.price}>2000</span>
+            <span className={classes.price}>{price}</span>
           </div>
         </div>
       </Card>
