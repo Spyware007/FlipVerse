050102bd1343563474cb83cec58f411d60fd27ab
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-07-27 17:35:57 +0530

    Fixed auth issues

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const setAuthToken = (token) => {
-  if (token) {
-    axios.defaults.headers.common["x-auth-token"] = token;
-  } else {
-    delete axios.defaults.headers.common["x-auth-token"];
-  }
+	console.log(axios.defaults);
+	if (token) {
+		axios.defaults.headers.common["x-auth-token"] = token;
+	} else {
+		delete axios.defaults.headers.common["x-auth-token"];
+	}
 };
 
 export default setAuthToken;
