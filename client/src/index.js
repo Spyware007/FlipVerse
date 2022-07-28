6dcbdc37f084b131ef8f76eae3d9db79826f7f4d
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-07-28 22:18:32 +0530

    Added web3 modules

--- client/src/index.js
+++ client/src/index.js
@@ -2,20 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { SellerState, UserState, ProductState } from "./Contexts";
+import { MoralisProvider } from "react-moralis";
 import { BrowserRouter } from "react-router-dom";
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 const app = (
-  <React.StrictMode>
-    <SellerState>
-      <UserState>
-        <ProductState>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </ProductState>
-      </UserState>
-    </SellerState>
-  </React.StrictMode>
+	<React.StrictMode>
+		<MoralisProvider initializeOnMount={false}>
+			<SellerState>
+				<UserState>
+					<ProductState>
+						<BrowserRouter>
+							<App />
+						</BrowserRouter>
+					</ProductState>
+				</UserState>
+			</SellerState>
+		</MoralisProvider>
+	</React.StrictMode>
 );
 root.render(app);
