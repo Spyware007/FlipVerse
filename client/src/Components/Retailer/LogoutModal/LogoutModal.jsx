2cb6eda486eb45eab67a66d71f9cce68da957698
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-07-28 11:17:16 +0530

    minor changes in retailer profile

--- client/src/Components/Retailer/LogoutModal/LogoutModal.jsx
+++ client/src/Components/Retailer/LogoutModal/LogoutModal.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button, Modal } from "../../UI";
 import classes from "./LogoutModal.module.css";
+import { sellerAuthContext } from "../../../Contexts";
 
 const LogoutModal = (props) => {
+  const { logoutSeller } = useContext(sellerAuthContext);
+
   return (
     <Modal onClose={props.onClose}>
       <h1 className={classes.text}>Are you sure you want to logout?</h1>
       <div className={classes.btns}>
-        <Button label="Logout" />
+        <Button label="Logout" onClick={logoutSeller} />
         <Button label="Cancel" filled onClick={props.onClose} />
       </div>
     </Modal>
